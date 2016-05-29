d5a2c8673cf73850ddc88cf9ecf506195fbd9e34
sirMackk <c.tairix@gmail.com>
2016-05-29 15:00:43 +0200

    Rewriting fold_lines; adding skeleton of delete comments

--- fold_lines.c
+++ fold_lines.c
@@ -1,7 +1,73 @@
 #include <stdio.h>
 
-/* Write a program to "fold" long input lines into two or more shorter
+#define MAXLINE 1000
+#define MAXCOL 14
+
+/* 1-22: Write a program to "fold" long input lines into two or more shorter
  * lines after the last non-blank character that occurs before the n-th
  * column of input. Make sure your program does something intelligent
  * with very long lines, and if there are no blanks or tabs before
  * the specified column. */
+
+/* 16-05-28 - Does not handle 'ragged' output well. */
+
+int getline(char line[], const int limit);
+void fold(const char line[], char output[], const int max_fold);
+void fold2(const char line[], char output[], const int max_fold);
+
+int main()
+{
+    int len;
+    char line[MAXLINE], output[MAXLINE];
+
+    while ((len = getline(line, MAXLINE)) != 0) {
+        fold2(line, output, MAXCOL);
+        printf("%s", output);
+    }
+
+    return 0;
+}
+
+int getline(char line[], const int limit)
+{
+    int i, c;
+
+    for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
+
+    if (c == '\n') {
+        line[i] = '\n';
+        ++i;
+    }
+
+    line[i] = '\0';
+
+    return i;
+}
+
+void fold(const char input[], char output[], const int max_fold)
+{
+    int i, j, k, last_blank;
+
+    i = j = k = last_blank = 0;
+
+    for (i = 0; i < MAXLINE && input[i] != '\0'; ++i) {
+        if (input[i] == ' ') {
+            last_blank = i;
+        }
+
+        if (++k > max_fold) {
+            printf("%d - %d - %d\n", i, k, last_blank);
+            if (last_blank == 0) {
+                output[j++] = '\n';
+                k = 0;
+            } else {
+                output[last_blank] = '\n';
+                k = k - last_blank;
+            }
+            last_blank = 0;
+        }
+        output[j++] = input[i];
+    }
+    output[j] = '\0';
+}
---
+++ remove_comments.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* 1-23: Write a program to remove all comments from a C program.
+ * Don't forget to handle quoted strings and character constants
+ * properly. C comments do not nest */
+
+/* state flag - start of comment stop copying, stop of comment - start copying */

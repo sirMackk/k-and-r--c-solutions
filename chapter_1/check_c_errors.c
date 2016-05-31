5bb6254226ca88c8700ae61d1888163d521e318a
sirMackk <c.tairix@gmail.com>
2016-05-31 11:01:01 +0200

    Finishes chapter 1 - except 1-24

---
+++ chapter_1/check_c_errors.c
@@ -0,0 +1,4 @@
+/* 1-24: Write a program to check a C program for rudimentary syntax
+ * errors like unbalanced parentheses, brackets, and braces. Dont
+ * forget about quotes, both single and double, escape sequences, and 
+ * comments. */

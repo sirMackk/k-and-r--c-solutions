7409f4ba6ecba99b01e413e329d3079076327629
sirMackk <c.tairix@gmail.com>
2016-09-01 12:32:11 +0200

    Breaks up 4_5 into multiple files + adds Makefile.

---
+++ chapter_4/calc/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "calc.h"
+#define MAXOP 100
+
+int main()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+            switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case 'p':
+                peek();
+                break;
+            case 'd':
+                dupe();
+                break;
+            case 's':
+                swap();
+                break;
+            case 'c':
+                clear_stack();
+                break;
+            case 'o':
+                print_stack();
+                break;
+            case 'i':
+                push(sin(pop()));
+                break;
+            case 'e':
+                push(exp(pop()));
+                break;
+            case 'l':
+                op2 = pop();
+                push(pow(op2, pop()));
+                break;
+            case '\n':
+                /*printf("\t%.8g\n", pop());*/
+                break;
+            default:
+                printf("error: unknown command '%s'\n", s);
+                break;
+            }
+    }
+    return 0;
+}
---
+++ chapter_4/calc/stack.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "calc.h"
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+
+
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+void peek(void)
+{
+    if (sp > 0)
+        printf("top of stack: %g\n", val[sp - 1]);
+    else
+        printf("error: stack empty\n");
+}
+
+void dupe(void)
+{
+    double tmp;
+    tmp = pop();
+    push(tmp);
+    push(tmp);
+}
+
+void swap(void)
+{
+    double item1, item2;
+    item1 = pop();
+    item2 = pop();
+    push(item1);
+    push(item2);
+}
+
+void clear_stack(void)
+{
+    int i;
+    for (i = 0; i < sp; i++)
+        val[i] = 0.0;
+
+    sp = 0;
+}
+
+void print_stack(void)
+{
+    int i;
+    if (sp > 0)
+        for (i = sp - 1; i >= 0; i--)
+            printf("%d: %g\n", sp - i, val[i]);
+    else
+        printf("error: stack empty\n");
+}

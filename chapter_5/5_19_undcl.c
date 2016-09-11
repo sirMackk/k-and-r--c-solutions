916329ce9351e5b9968a404a96ecd6016b90c5ea
sirMackk <c.tairix@gmail.com>
2016-09-11 18:45:12 +0200

    Finishes 5.19

---
+++ chapter_5/5_19_undcl.c
@@ -0,0 +1,159 @@
+/* 5-1l: Modify undcl so that it does not add redundent parentheses to declarations. */
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN 100
+
+enum { NAME, PARENS, BRACKETS };
+
+
+void dcl_to_words(void);
+int gettoken(void);
+void dcl(void);
+void dirdcl(void);
+void undcl(void);
+int getch(void);
+void ungetch(int);
+
+int tokentype;
+char token[MAXTOKEN];
+char name[MAXTOKEN];
+char datatype[MAXTOKEN];
+char out[1000];
+
+#define BUFSIZE 100
+
+static char buf[BUFSIZE];
+static int bufp = 0;
+
+int main(int argc, char *argv[])
+{
+    /*dcl_to_words();*/
+    undcl();
+    return 0;
+}
+
+void dcl_to_words(void)
+{
+    while (gettoken() != EOF) {
+        strcpy(datatype, token);
+        out[0] = '\0';
+        dcl();
+        if (tokentype != '\n')
+            printf("syntax error\n");
+        printf("%s: %s %s\n", name, out, datatype);
+    }
+}
+
+int gettoken(void)
+{
+    int c;
+    char *p = token;
+
+    while ((c = getch()) == ' ' || c == '\t')
+        ;
+
+    if (c == '(') {
+        if ((c = getch()) == ')') {
+            strcpy(token, "()");
+            return tokentype = PARENS;
+        } else {
+            ungetch(c);
+            return tokentype = '(';
+        }
+    } else if (c == '[') {
+        for (*p++ = c; (*p++ = getch()) != ']'; )
+            ;
+        *p = '\0';
+        return tokentype = BRACKETS;
+    } else if (isalpha(c)) {
+        for (*p++ = c; isalnum(c = getch()); )
+            *p++ = c;
+        *p = '\0';
+        ungetch(c);
+        return tokentype = NAME;
+    } else {
+        return tokentype = c;
+    }
+}
+
+void dcl(void)
+{
+    int ns;
+
+    for (ns = 0; gettoken() == '*'; ) {
+        ns++;
+    }
+
+    dirdcl();
+    while (ns-- > 0)
+        strcat(out, " pointer to ");
+}
+
+void dirdcl(void)
+{
+    int type;
+
+    if (tokentype == '(') {
+        dcl();
+        if (tokentype != ')')
+            printf("error: missing )\n");
+    } else if (tokentype == NAME) {
+        strcpy(name, token);
+    } else {
+        printf("error: expected name or (dcl)\n");
+    }
+
+    while ((type = gettoken()) == PARENS || type == BRACKETS)
+        if (type == PARENS)
+            strcat(out, "function returning");
+        else {
+            strcat(out, "array");
+            strcat(out, token);
+            strcat(out, " of");
+        }
+}
+
+
+void undcl(void)
+{
+    int type, last_type;
+    char temp[MAXTOKEN];
+
+    while (gettoken() != EOF) {
+        strcpy(out, token);
+        while ((type = gettoken()) != '\n') {
+            if (type == PARENS || type == BRACKETS) {
+                if (last_type == '*')
+                    strcat(out, token);
+            } else if (type == '*') {
+                sprintf(temp, "(*%s)", out);
+                strcpy(out, temp);
+            } else if (type == NAME) {
+                sprintf(temp, "%s %s", token, out);
+                strcpy(out, temp);
+            } else {
+                printf("invalid input at %s\n", token);
+            }
+
+            last_type = type;
+        }
+        printf("%s\n", out);
+    }
+}
+
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
